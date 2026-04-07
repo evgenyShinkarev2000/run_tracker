e5f243e48c687f2e6bcf60643007fc2cdbfbf45a
e.shinkarev <praktika.summer@mail.ru>
2026-04-07 14:54:23 +0500

    Исправлена иконка, ломавшая tree shaking

--- lib/Components/CustomIcons/CustomIcons.dart
+++ lib/Components/CustomIcons/CustomIcons.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const String _familyName = "CustomIcons";
 
 class CustomIcons {
-  static const IconData place_no_dot = IconData(0xe800, fontFamily: _familyName);
+  static const IconData place_no_dot = IconData(0xe900, fontFamily: _familyName);
 }
